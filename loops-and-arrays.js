768c79492f818efbcc535851ddb229b5e7477808
Olafvdberg <olafvdberg@hotmail.com>
2022-01-18 13:54:26 +0100

    huiswerk gemaakt

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -12,7 +12,11 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+console.log(names)
 
+for (let i =0; i <names.length;i++) {
+    console.log(names[i] + "je")
+}
 
 
 // ==========================================
@@ -22,6 +26,17 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // ==========================================
 
 const numbers = [2, 4, 5, 29, 38];
+
+console.log(numbers)
+
+for (let i=0; i < numbers.length; i++) {
+    if (numbers[i]% 2 == 0){
+        console.log(numbers[i] * 2)
+    }
+    else {
+        console.log(numbers[i] *3)
+    }
+}
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -42,6 +57,10 @@ const numbers = [2, 4, 5, 29, 38];
 
 const squares = [30, 2, 8, 24, 11];
 
+console.log(squares)
+
+for (let i =0; i <squares.length; i++)
+    console.log("Het volume van " + squares[i] + " is " + squares[i] * squares [i] *squares[i])
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(squares) geeft: [30, 2, 8, 24, 11];
@@ -53,3 +72,4 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
